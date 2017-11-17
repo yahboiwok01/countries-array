2321c19bf9e12dce7383ec4c6adbf1c2cc66d419
ivan <ivana50272@students.laalliance.org>
2017-11-17 20:46:47 +0000

    I'm done with my lab.

---
+++ countries.js
@@ -0,0 +1,14 @@
+var fourLetterCountries = [
+    "Chad", 
+    "Cuba",
+    "Greenland",
+    "Iraq",
+    "Mali",
+    "Oman",
+    "India"
+]
+fourLetterCountries.push('Fiji')
+fourLetterCountries.splice(6, 1)
+
+console.log(fourLetterCountries)
+ 
